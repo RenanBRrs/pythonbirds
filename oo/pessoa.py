f53d339f33c844809ee77cc823e87b30a4e12130
Renan Brito <renandossantosbrito@hotmail.com>
2022-05-26 20:50:56 -0300

    criados atributos de instancia, nome

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,10 @@
 class Pessoa:
+    def __init__(self, nome=None):
+        self.nome = nome
+
     def cumprimentar(self):
         return 'Ola mundo'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(p.cumprimentar())
+    p = Pessoa('Renan')
+    print(p.cumprimentar() + f' e Oi {p.nome}')
